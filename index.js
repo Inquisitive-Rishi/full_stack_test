55347c21d49447b22a58c32e3fcb79d364ba462f
Rishi Raj <rishiraj007bgp@gmail.com>
2023-12-14 15:11:31 +0530

    correct mistype

--- index.js
+++ index.js
@@ -92,6 +92,6 @@ app.delete('/api/persons/:id', (req, res) => {
   console.log(persons);
 })
 
-const PORT = proces.env || 3001;
+const PORT = process.env || 3001;
 app.listen(PORT)
 console.log(`server is listening to port ${PORT}`);
